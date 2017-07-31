31bff08fc9d4e21f9e43ae797acbad19c6c033ae
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-30 21:56:52 -0500

    basic users api

--- seeders/20170731022725-users.js
+++ seeders/20170731022725-users.js
@@ -1,12 +1,24 @@
 'use strict';
+const casual = require('casual');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
+    let users = [];
 
-      return queryInterface.bulkInsert('Users', [{
-        name: 'John',
-        lastname: 'Doe'
-      }], {});
+    for (var i = 0; i < 20; i++) {
+      users = users.concat({
+        name: casual.first_name,
+        lastname: casual.last_name,
+        email: casual.email,
+        password: casual.password,
+        city: casual.city,
+        age: casual.integer(18, 80),
+        height: casual.double(1, 2),
+        weight: casual.integer(30, 100),
+      });
+    }
+
+      return queryInterface.bulkInsert('Users', users, {});
   },
 
   down: function (queryInterface, Sequelize) {
